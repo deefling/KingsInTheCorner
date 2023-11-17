f86ecca93bead175aade137a9ab1f53f15f15fc5
Max <mas5627@rit.edu>
2023-11-17 14:41:28 -0500

    Added .env & express dependencies. Started serving html login page with express

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-// public domain cards https://tekeye.uk/playing_cards/svg-playing-cards, includes card backs
-console.log("hello world")
+//setup basic service of html pages?
+
+const express = require('express')
+const server = express()
+const port = 3000
+const path = require('path');
+const dotenv = require('dotenv').config()
+const pages_dir = process.env.PAGES_DIRECTORY
+
+
+
+server.get('/login', (req, res) => {
+//   res.send('Hello World!')
+  res.sendFile(path.join(pages_dir, '/login.html'));
+
+})
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
