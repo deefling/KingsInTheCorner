5112b2d3d5ce0c04646da6bdbbc820fe682cf478
Max <mas5627@rit.edu>
2023-12-05 21:17:30 -0500

    setup endpoint to check user passwords - struggling with race condition

--- assets/drivers/mySQLDriver.js
+++ assets/drivers/mySQLDriver.js
@@ -1,45 +1,111 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
+const {sha256} = require('js-sha256');
 
 //CREATE USER 'King'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Corner';
 // GRANT ALL on *.* to 'King'@'localhost';
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "King",
-    password: "Corner"
-  });
-  
-  
+var pool = mysql.createPool({
+  connectionLimit : 10,
+  host: "localhost",
+  user: "King",
+  password: "Corner",
+  database: 'kingsinthecorner'
+});
+
 
 function recreateDB(){
-    con.connect(function(err) {
+  pool.getConnection(function(err, con) {
+    if (err) throw err;
+        
+    //USE DB
+    con.query("CREATE DATABASE IF NOT EXISTS kingsinthecorner", function (err, result) {
         if (err) throw err;
-        console.log("Connected!");
+        dbLog("Database created/found");
+    });
     
-        //USE DB
-        con.query("CREATE DATABASE IF NOT EXISTS kingsinthecorner", function (err, result) {
-            if (err) throw err;
-            console.log("Database created/found");
-        });
-        
-        //USE DB
-        con.query("USE kingsinthecorner", function (err, result) {
-            if (err) throw err;
-            console.log("Database accessed");
-        });
-    
-        //DROP USER TABLE
-        con.query("DROP TABLE IF EXISTS users", function (err, result) {
-            if (err) throw err;
-            console.log("Table dropped");
-        });
-        
-        //CREATE TABLE FOR USERS
-        var sql = "CREATE TABLE users (username VARCHAR(255), password VARCHAR(255))";
-        con.query(sql, function (err, result) {
-          if (err) throw err;
-          console.log("Table created");
-        });
-      });
+    //USE DB
+    con.query("USE kingsinthecorner", function (err, result) {
+        if (err) throw err;
+        dbLog("Database accessed");
+    });
+
+    //DROP USER TABLE
+    con.query("DROP TABLE IF EXISTS users", function (err, result) {
+        if (err) throw err;
+        dbLog("Table dropped");
+    });
+      
+    //CREATE TABLE FOR USERS
+    //TODO add session token & login time?
+    var sql = "CREATE TABLE users (username VARCHAR(255), password VARCHAR(255))";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      dbLog("Table created");
+      
+      //INSERT SAMPLE USER
+      addUser('deefling', 'password')
+    });
+
+    con.release()
+  });
+  
 }
 
-recreateDB()
+function addUser(user, pw) {
+  pool.getConnection(function(err, con) {
+    if (err) throw err;
+    dbLog("Connected!");
+
+    const password = sha256(pw)
+    
+    var sql = "INSERT INTO users (username, password) VALUES (?,?)";
+        con.query(sql, [user, password], (err, result) => {
+          if (err) throw err;
+          dbLog("Record inserted!");
+    });
+
+    con.release()
+  });
+}
+
+function checkUser(user, pw){
+  var valid = false;
+  
+  pool.getConnection(function(err, con) {
+    if (err) throw err;
+
+    const password = sha256(pw)
+  
+    var sql = "SELECT username FROM users WHERE username = ? AND password = ?;"
+    
+    con.query(sql, [user, password], (err, result) => {
+      if (err) throw err;
+
+      if(result.length == 0){
+        dbLog('Matching user & password not found');
+        return;
+      }
+
+      dbLog("User password confirmed");
+      valid = true
+      return
+    });
+
+    con.release();
+  });
+  return valid;
+}
+
+function checkToken(user, token){
+
+}
+
+function dbLog(msg){
+  console.log('\x1b[34m[MySQL]:\x1b[0m', msg)
+}
+
+
+
+
+// recreateDB();
+
+module.exports = { checkUser, dbLog}
--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-//setup basic service of html pages?
-
 const express = require('express')
+var cors = require('cors')
 const server = express()
 const port = 3000
 const path = require('path');
 const dotenv = require('dotenv').config()
 const pages_dir = process.env.PAGES_DIRECTORY
+const sql = require('./assets/drivers/mySQLDriver')
+
+// server.use(cors({origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'null', '*']}))
+
+
+
+server.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', ['origin', 'Content-Type', 'x-api-key', 'Accept']);
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    apiLog(req.url)
+    next();
+});
+
+server.use(express.json());
+
+
+
 
 
 
 server.get('/login', (req, res) => {
   res.sendFile(path.join(pages_dir, '/login.html'));
-
 })
+
+server.post('/checkPassword', (req, res) => {
+    const valid = sql.checkUser(req.body.username, req.body.password)
+    apiLog(valid)
+    res.json({validUser: valid});
+    // res.send(valid)
+  });
 
 server.get('/lobby', (req, res) => {
     res.sendFile(path.join(pages_dir, '/lobby.html'));
@@ -24,3 +48,7 @@ server.get('/lobby', (req, res) => {
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
+
+function apiLog(msg){
+  console.log('\x1b[95m[Server]:\x1b[0m', msg)
+}
